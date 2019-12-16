3766516af72853331ca972422e14fa600de4c3c2
Humberto Leal <humberto.adolfo@hotmail.com>
2019-12-16 11:52:17 -0500

    SDA and SCL global variable used for default test

--- esp32-i2c-scanner/src/main.cpp
+++ esp32-i2c-scanner/src/main.cpp
@@ -1,42 +1,51 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-void setup() {
-  // Hay pines que no funcionan en el t-beam. Consultar lista personal.
-  Wire.begin(0, 4);
-  Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
+void setup()
+{
+    // Hay pines que no funcionan en el t-beam. Consultar lista personal.
+    Wire.begin(SDA, SCL);
+    Serial.begin(115200);
+    Serial.println("\nI2C Scanner");
 }
- 
-void loop() {
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-      nDevices++;
+
+void loop()
+{
+    byte error, address;
+    int nDevices;
+    Serial.println("Scanning...");
+    nDevices = 0;
+    for (address = 1; address < 127; address++)
+    {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+        if (error == 0)
+        {
+            Serial.print("I2C device found at address 0x");
+            if (address < 16)
+            {
+                Serial.print("0");
+            }
+            Serial.println(address, HEX);
+            nDevices++;
+        }
+        else if (error == 4)
+        {
+            Serial.print("Unknow error at address 0x");
+            if (address < 16)
+            {
+                Serial.print("0");
+            }
+            Serial.println(address, HEX);
+        }
     }
-    else if (error==4) {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  }
-  else {
-    Serial.println("done\n");
-  }
-  delay(5000);          
+    if (nDevices == 0)
+    {
+        Serial.println("No I2C devices found\n");
+    }
+    else
+    {
+        Serial.println("done\n");
+    }
+    delay(5000);
 }
